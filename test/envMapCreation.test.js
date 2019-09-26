cdda2a48c891b5db9dd66427e67c131ca8c653e3
Noah Lyons <n.lyons53@gmail.com>
2019-09-26 16:33:17 -0700

    Updated tests

--- test/envMapCreation.test.js
+++ test/envMapCreation.test.js
@@ -2,26 +2,27 @@ import { equirectangularToSpherical, getAngleDelta } from 'src/renderer/envMapCr
 import * as THREE from 'three';
 
 describe('equirectangularToSpherical', () => {
-  let oddWidth, oddHeight, evenWidth, evenHeight;
+  let oddWidth, oddHeight, evenWidth, evenHeight, targetCoords;
   beforeEach(() => {
     oddWidth = 11;
     evenWidth = 10;
     oddHeight = 11;
     evenHeight = 10;
+    targetCoords = new THREE.Spherical();
   });
   test('converts center of map to theta = PI and phi = PI/2', () => {
-    let coords = equirectangularToSpherical(oddWidth / 2, oddHeight / 2, oddWidth, oddHeight);
+    let coords = equirectangularToSpherical(oddWidth / 2, oddHeight / 2, oddWidth, oddHeight, targetCoords);
     expect(coords.theta).toBeCloseTo(Math.PI);
     expect(coords.phi).toBeCloseTo(Math.PI / 2);
-    coords = equirectangularToSpherical(evenWidth / 2, evenHeight / 2, evenWidth, evenHeight);
+    coords = equirectangularToSpherical(evenWidth / 2, evenHeight / 2, evenWidth, evenHeight, targetCoords);
     expect(coords.theta).toBeCloseTo(Math.PI);
     expect(coords.phi).toBeCloseTo(Math.PI / 2);
   });
   test('converts lower left corner of map to theta = 0 and phi = 0', () => {
-    let coords = equirectangularToSpherical(0, 0, oddWidth, oddHeight);
+    let coords = equirectangularToSpherical(0, 0, oddWidth, oddHeight, targetCoords);
     expect(coords.theta).toBeCloseTo(0);
     expect(coords.phi).toBeCloseTo(0);
-    coords = equirectangularToSpherical(0, 0, evenWidth, evenHeight);
+    coords = equirectangularToSpherical(0, 0, evenWidth, evenHeight, targetCoords);
     expect(coords.theta).toBeCloseTo(0);
     expect(coords.phi).toBeCloseTo(0);
   });
