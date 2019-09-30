b0cc3166f555ceb2fd2751a6026225c8fd80a555
Noah Lyons <n.lyons53@gmail.com>
2019-09-30 16:34:29 -0700

    add comment explaining bit shift

--- src/renderer/rgbeToFloat.js
+++ src/renderer/rgbeToFloat.js
@@ -4,7 +4,7 @@
 export function rgbeToFloat(buffer, multiplier = 1) {
   const texels = buffer.length / 4;
   const floatBuffer = new Float32Array(texels * 3);
-  const imageCoords = [0, 0];
+
   for (let i = 0; i < texels; i++) {
 
     const r = buffer[4 * i];
@@ -13,6 +13,7 @@ export function rgbeToFloat(buffer, multiplier = 1) {
     const a = buffer[4 * i + 3];
 
     const exponent = a - 128;
+    // bit shift equivilent to e = Math.pow(2, exponent);
     const e = exponent > 0 ? 1 << exponent : 1 / (1 << (-1 * exponent));
 
     floatBuffer[3 * i] = multiplier * r * e / 255;
