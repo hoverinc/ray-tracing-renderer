82d7f8b8fd7e1a22c33911c646ec749e4e04a2bb
Noah Lyons <noah@hover.to>
2019-10-05 14:06:49 -0700

    Optimize Envmap Creation (#17)
    * Optimize Envmap Creation
    
    Change RGBEToFloat exponentiation to bit shift
    
    Add multiplier to RGBEToFloat to optimizing environment light intensity adjustment
    
    Implement early bail on addLightAtCoordinates once last light texel has been added
    
    Optimize out most 3d angle calculations by implementing a threshold on theta and phi
    
    * simplify to logical operator for softness variable assignment
    
    * improve thresholding cuttoff
    
    * Don't apply threshold if light radius is too large, not enough texels will be skipped to make up for an extra conditional check on each texel
    
    optimize out GC from equirectangularToSpherical
    
    * cleanup
    
    * Updated tests
    
    * add comments
    
    * add comment explaining bit shift
    
    * replace function declaration with function expression to avoid re-declaration of variables
    
    * improve variable names and add comments for clarity

--- src/renderer/envMapCreation.js
+++ src/renderer/envMapCreation.js
@@ -1,3 +1,6 @@
+// Convert image data from the RGBE format to a 32-bit floating point format
+// See https://www.cg.tuwien.ac.at/research/theses/matkovic/node84.html for a description of the RGBE format
+
 import { rgbeToFloat } from './rgbeToFloat';
 import { clamp } from './util';
 import * as THREE from 'three';
@@ -26,10 +29,7 @@ export function initializeEnvMap(environmentLights) {
       height: environmentLight.map.image.height,
       data: environmentLight.map.image.data,
     };
-    envImage.data = rgbeToFloat(envImage.data);
-    envImage.data.forEach((datum, index, arr) => {
-      arr[index] = datum * environmentLight.intensity;
-    });
+    envImage.data = rgbeToFloat(envImage.data, environmentLight.intensity);
   } else { // initialize blank map
     envImage = generateBlankMap(DEFAULT_MAP_RESOLUTION.width, DEFAULT_MAP_RESOLUTION.height);
   }
@@ -59,28 +59,81 @@ export function addDirectionalLightToEnvMap(light, image) {
 }
 
 // Perform modifications on env map to match input scene
-function addLightAtCoordinates(light, image, originSphericalCoords) {
+function addLightAtCoordinates(light, image, originCoords) {
   const floatBuffer = image.data;
   const width = image.width;
   const height = image.height;
-
   const xTexels = (floatBuffer.length / (3 * height));
   const yTexels = (floatBuffer.length / (3 * width));
-  // default softness for standard directional lights is 0.95
-  const softness = ("softness" in light && light.softness !== null) ? light.softness : 0.45;
+  const density = 8;
+  // default softness for standard directional lights is 0.01, i.e. a hard shadow
+  const softness = light.softness || 0.01;
+
+  // angle from center of light at which no more contributions are projected
+  const threshold = findThreshold(originCoords, softness);
+  // if too few texels are rejected by the threshold then the time to evaluate it is no longer worth it
+  const useThreshold = threshold < Math.PI / 5;
+  // functional trick to keep the conditional check out of the main loop
+  const intensityFromAngleFunction = useThreshold ? getIntensityFromAngleDifferentialThresholded : getIntensityFromAngleDifferential;
+
+  let encounteredInThisRow = false;
+  let begunAddingContributions = false;
+  let currentCoords = new THREE.Spherical();
+  let falloff,
+      intensity,
+      bufferIndex;
+
+  // Iterates over each row from top to bottom
   for (let i = 0; i < xTexels; i++) {
+    // Iterates over each texel in row
     for (let j = 0; j < yTexels; j++) {
-      const bufferIndex = j * width + i;
-      const currentSphericalCoords = equirectangularToSpherical(i, j, width, height);
-      const falloff = getIntensityFromAngleDifferential(originSphericalCoords, currentSphericalCoords, softness);
-      const intensity = light.intensity * falloff;
+      bufferIndex = j * width + i;
+      currentCoords = equirectangularToSpherical(i, j, width, height, currentCoords);
+      falloff = intensityFromAngleFunction(originCoords, currentCoords, softness, threshold);
+
+      if(falloff > 0) {
+        encounteredInThisRow = true;
+        begunAddingContributions = true;
+      }
+      intensity = light.intensity * falloff;
 
       floatBuffer[bufferIndex * 3] += intensity * light.color.r;
       floatBuffer[bufferIndex * 3 + 1] += intensity * light.color.g;
       floatBuffer[bufferIndex * 3 + 2] += intensity * light.color.b;
     }
+    // First row to not add a contribution since adding began
+    // This means the entire light has been added and we can exit early
+    if(!encounteredInThisRow && begunAddingContributions) {
+      return floatBuffer;
+    }
+    // reset for next row
+    encounteredInThisRow = false;
   }
   return floatBuffer;
+}
+
+function findThreshold(originCoords, softness) {
+  const step = Math.PI / 128;
+  const maxSteps = (2.0 * Math.PI) / step;
+  for (let i = 0; i < maxSteps; i++) {
+    const angle = i * step;
+    const falloff = getFalloffAtAngle(angle, softness);
+    if (falloff <= 0.0001) {
+      return angle;
+    }
+  }
+}
+
+function getIntensityFromAngleDifferentialThresholded(originCoords, currentCoords, softness, threshold) {
+  let deltaPhi = getAngleDelta(originCoords.phi, currentCoords.phi);
+  let deltaTheta =  getAngleDelta(originCoords.theta, currentCoords.theta);
+
+  if(deltaTheta > threshold && deltaPhi > threshold) {
+    return 0;
+  }
+  const angle = angleBetweenSphericals(originCoords, currentCoords);
+  const falloffCoeficient = getFalloffAtAngle(angle, softness);
+  return falloffCoeficient;
 }
 
 function getIntensityFromAngleDifferential(originCoords, currentCoords, softness) {
@@ -89,24 +142,38 @@ function getIntensityFromAngleDifferential(originCoords, currentCoords, softness
   return falloffCoeficient;
 }
 
-function angleBetweenSphericals(originCoords, currentCoords) {
-  const originVector = new THREE.Vector3();
-  originVector.setFromSpherical(originCoords);
-  const currentVector = new THREE.Vector3();
-  currentVector.setFromSpherical(currentCoords);
-  return originVector.angleTo(currentVector);
+export function getAngleDelta(angleA, angleB) {
+  let diff = Math.abs(angleA - angleB) % (2 * Math.PI);
+  return diff > Math.PI ? (2 * Math.PI - diff) : diff;
 }
 
+const angleBetweenSphericals = function() {
+  const originVector = new THREE.Vector3();
+  const currentVector = new THREE.Vector3();
+  const getAngle = function(originCoords, currentCoords) {
+    originVector.setFromSpherical(originCoords);
+    currentVector.setFromSpherical(currentCoords);
+    return originVector.angleTo(currentVector);
+  }
+  return getAngle;
+}();
+
+  // TODO: possibly clean this up and optimize it
+  //
+  // This function was arrived at through experimentation, it provides good 
+  // looking results with percieved softness that scale relatively linearly with
+  //  the softness value in the 0 - 1 range
+  //
+  // For now it doesn't incur too much of a performance penalty because for most of our use cases (lights without too much softness)
+  // the threshold cutoff in getIntensityFromAngleDifferential stops us from running it too many times
 function getFalloffAtAngle(angle, softness) {
   const softnessCoeficient = Math.pow(2, 14.5 * Math.max(0.001, (1.0 - clamp(softness, 0.0, 1.0))));
   const falloff = Math.pow(softnessCoeficient, 1.1) * Math.pow(8, softnessCoeficient * -1 * (Math.pow(angle, 1.8)));
   return falloff;
 }
 
-export function equirectangularToSpherical(x, y, width, height) {
-  const TWOPI = 2.0 * Math.PI;
-  const theta = (TWOPI * x) / width;
-  const phi = (Math.PI * y) / height;
-  const sphericalCoords = new THREE.Spherical(1.0, phi, theta);
-  return sphericalCoords;
+export function equirectangularToSpherical(x, y, width, height, target) {
+  target.phi = (Math.PI * y) / height;
+  target.theta = (2.0 * Math.PI * x) / width;
+  return target;
 }
--- src/renderer/rgbeToFloat.js
+++ src/renderer/rgbeToFloat.js
@@ -1,18 +1,24 @@
 // Convert image data from the RGBE format to a 32-bit floating point format
 // See https://www.cg.tuwien.ac.at/research/theses/matkovic/node84.html for a description of the RGBE format
-export function rgbeToFloat(buffer) {
+// Optional multiplier argument for performance optimization
+export function rgbeToFloat(buffer, multiplier = 1) {
   const texels = buffer.length / 4;
   const floatBuffer = new Float32Array(texels * 3);
 
   for (let i = 0; i < texels; i++) {
+
     const r = buffer[4 * i];
     const g = buffer[4 * i + 1];
     const b = buffer[4 * i + 2];
     const a = buffer[4 * i + 3];
-    const e = 2 ** (a - 128);
-    floatBuffer[3 * i] = r * e / 255;
-    floatBuffer[3 * i + 1] = g * e / 255;
-    floatBuffer[3 * i + 2] = b * e / 255;
+
+    const exponent = a - 128;
+    // bit shift equivilent to e = Math.pow(2, exponent);
+    const e = exponent > 0 ? 1 << exponent : 1 / (1 << (-1 * exponent));
+
+    floatBuffer[3 * i] = multiplier * r * e / 255;
+    floatBuffer[3 * i + 1] = multiplier * g * e / 255;
+    floatBuffer[3 * i + 2] = multiplier * b * e / 255;
   }
 
   return floatBuffer;
