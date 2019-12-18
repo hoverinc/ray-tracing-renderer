bc700d48518bdb56b85fd8e86e861e3379fdf2e9
jaxry <cabot696@gmail.com>
2019-12-17 21:08:33 -0800

    smarter texture binding

--- src/renderer/TextureAllocator2.js
+++ src/renderer/TextureAllocator2.js
@@ -2,7 +2,9 @@ export function makeTextureAllocator2(gl) {
 
   const maxUnits = gl.getParameter(gl.MAX_COMBINED_TEXTURE_IMAGE_UNITS);
 
-  const shaderUnitMap = new WeakMap();
+  const shaderMap = new WeakMap();
+
+  const textureMap = new WeakMap();
 
   const availableUnits = [];
 
@@ -13,42 +15,47 @@ export function makeTextureAllocator2(gl) {
   function bind(shaderPass) {
     const textures = shaderPass.textures;
 
-    let shaderUnits = shaderUnitMap.get(textures);
+    let lastUsedTextures = shaderMap.get(textures);
 
-    if (!shaderUnits) {
-      shaderUnits = {};
-      for (let name in textures) {
-        shaderUnits[name] = { unit: -1, tex: null };
-      }
-      shaderUnitMap.set(shaderPass.textures, shaderUnits);
+    if (!lastUsedTextures) {
+      lastUsedTextures = {};
+      shaderMap.set(textures, lastUsedTextures);
     }
 
     for (let name in textures) {
       const tex = shaderPass.textures[name];
 
-      let unit;
-
-      if (shaderUnits[name].tex) {
-        if (shaderUnits[name].tex === tex) {
+      if (lastUsedTextures[name]) {
+        const lastTex = lastUsedTextures[name];
+        if (lastTex === tex) {
           return;
         } else {
-          const oldUnit = shaderUnits[name].unit;
+          const lastTextureMap = textureMap.get(lastTex);
+          lastTextureMap.count--;
 
-          unit = availableUnits.pop();
-          shaderUnits[name].unit = unit;
-          shaderUnits[name].tex = tex;
-
-          availableUnits.push(oldUnit);
+          if (lastTextureMap.count <= 0) {
+            availableUnits.push(lastTextureMap.unit);
+          }
         }
-      } else {
-        unit = availableUnits.pop();
-        shaderUnits[name].unit = unit;
-        shaderUnits[name].tex = tex;
       }
 
-      gl.activeTexture(gl.TEXTURE0 + unit);
+      lastUsedTextures[name] = tex;
+
+      let activeTexture = textureMap.get(tex);
+      if (activeTexture) {
+        activeTexture.count++;
+      } else {
+        activeTexture = {
+           unit: availableUnits.pop(),
+           count: 1
+        };
+
+        textureMap.set(tex, activeTexture);
+      }
+
+      gl.activeTexture(gl.TEXTURE0 + activeTexture.unit);
       gl.bindTexture(tex.target, tex.texture);
-      gl.uniform1i(shaderPass.uniforms[name], unit);
+      gl.uniform1i(shaderPass.uniforms[name], activeTexture.unit);
     }
   }
 
