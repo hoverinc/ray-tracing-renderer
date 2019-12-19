e4397dd4ad346897425d7d516dd4f49406002675
jaxry <cabot696@gmail.com>
2019-12-18 17:12:22 -0800

    just use simple texture allocation

--- src/renderer/TextureAllocator2.js
+++ src/renderer/TextureAllocator2.js
@@ -1,61 +1,16 @@
 export function makeTextureAllocator2(gl) {
-
-  const maxUnits = gl.getParameter(gl.MAX_COMBINED_TEXTURE_IMAGE_UNITS);
-
-  const shaderMap = new WeakMap();
-
-  const textureMap = new WeakMap();
-
-  const availableUnits = [];
-
-  for (let i = 1; i < maxUnits; i++) {
-    availableUnits.push(i);
-  }
-
   function bind(shaderPass) {
     const textures = shaderPass.textures;
 
-    let lastUsedTextures = shaderMap.get(textures);
-
-    if (!lastUsedTextures) {
-      lastUsedTextures = {};
-      shaderMap.set(textures, lastUsedTextures);
-    }
-
+    let i = 0;
     for (let name in textures) {
       const tex = shaderPass.textures[name];
 
-      if (lastUsedTextures[name]) {
-        const lastTex = lastUsedTextures[name];
-        if (lastTex === tex) {
-          return;
-        } else {
-          const lastTextureMap = textureMap.get(lastTex);
-          lastTextureMap.count--;
+      let unit = 30 + i++;
 
-          if (lastTextureMap.count <= 0) {
-            availableUnits.push(lastTextureMap.unit);
-          }
-        }
-      }
-
-      lastUsedTextures[name] = tex;
-
-      let activeTexture = textureMap.get(tex);
-      if (activeTexture) {
-        activeTexture.count++;
-      } else {
-        activeTexture = {
-           unit: availableUnits.pop(),
-           count: 1
-        };
-
-        textureMap.set(tex, activeTexture);
-      }
-
-      gl.activeTexture(gl.TEXTURE0 + activeTexture.unit);
+      gl.activeTexture(gl.TEXTURE0 + unit);
       gl.bindTexture(tex.target, tex.texture);
-      gl.uniform1i(shaderPass.uniforms[name], activeTexture.unit);
+      gl.uniform1i(shaderPass.uniforms[name], unit);
     }
   }
 
